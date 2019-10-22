291519834acc1d5ed5718f3ace77ef8ca5c46e78
Lenovo <Lenovo@DESKTOP-G5JUJVB>
2019-10-22 23:43:03 +0500

    firstcommit

---
+++ ExpenseApp/DAL/Dbconn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExpenseApp.DAL
+{
+    class Dbconn
+    {
+        public SQLiteConnection dbconn()
+        {
+            string[] paths = { @"" + Application.StartupPath + "", "expensedb.sqllite.db" };
+            string dbfilepath = Path.Combine(paths);
+
+            var conn = new SQLiteConnection("Data Source=" + dbfilepath + ";Version=3;");
+
+            return conn;
+        }
+    }
+}
---
+++ ExpenseApp/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExpenseApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            //const string filename = @"C:\cplus\tutorials\c#\SQLite\MyDatabase.sqlite";
+         
+            try
+            {
+                DAL.Dbconn obj = new DAL.Dbconn();
+
+                var conn=obj.dbconn();
+                conn.Open();
+                string sql = "select * from tbl_records";
+                DataSet ds = new DataSet();
+                var da = new SQLiteDataAdapter(sql, conn);
+                da.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0].DefaultView;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
